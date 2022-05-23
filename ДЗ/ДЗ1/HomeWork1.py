5fb7b135759c88ab03c8561be46b7b8ae5dc312f
Vsevolod <sevakuzya@mail.ru>
2022-05-23 09:54:25 +0300

    task2 done

--- ДЗ/ДЗ1/HomeWork1.py
+++ ДЗ/ДЗ1/HomeWork1.py
@@ -13,6 +13,29 @@
 #
 # day = int(input('Введите день: '))
 # Weekend(day)
+# 2. Напишите программу, которая принимает на вход координаты точки (X и Y),
+# и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+# def Getquart (xPoint, yPoint):
+#     if (xPoint > 0 and yPoint > 0):
+#         return 'Первая четверть'
+#     if (xPoint < 0 and yPoint > 0):
+#         return 'Вторая четверть'
+#     if (xPoint < 0 and yPoint < 0):
+#         return 'Третья четверть'
+#     if (xPoint > 0 and yPoint < 0):
+#         return 'Четвертая четверть'
+#     if (xPoint == 0 or yPoint == 0):
+#         return 'Точка находится на оси'
+#
+# def GetValues ():
+#     x = int(input('Введите координаты точки по X: '))
+#     y = int(input('Введите координаты точки по Y: '))
+#     return x, y
+#
+# x, y = GetValues()
+# print(Getquart(x,y))
+
+
 
 # 4. Напишите программу, которая принимает на вход координаты двух точек и находит
 # расстояние между ними в 2D пространстве.
@@ -27,5 +50,3 @@
 # yB = int(input('Введите координаты B по Y: '))
 #
 # print(Rasst2D(xA, yA, xB, yB))
-
-
