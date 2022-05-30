75dfa10efb6edb4dd9685b7f256311b772768e0b
Vsevolod <sevakuzya@mail.ru>
2022-05-30 15:27:24 +0300

    2 task of homework 3 is done

--- ДЗ/ДЗ3/Homework3.py
+++ ДЗ/ДЗ3/Homework3.py
@@ -15,3 +15,23 @@
 #     return summa
 #
 # print(summa_nechetnyh([2, 3, 5, 9, 3, 4, 5]))
+
+# 2.Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Если остается 1 элемент без пары - умножаем его самого на себя.
+#
+# Пример:
+#
+# [2, 3, 4, 5, 6] => [12, 15, 16];
+# [2, 3, 5, 6] => [12, 15]
+# import math
+# def proizved_par (spisok):
+#     new_list = []
+#     for i in range(math.ceil(len(spisok)/2)):
+#         new_list.append(spisok[i]*spisok[-i-1])
+#     return new_list
+#
+# print(proizved_par([2, 3, 4, 5, 6]))
+
+
+
