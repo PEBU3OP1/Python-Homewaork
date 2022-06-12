4357e4b3fbf5c4556c36d2c0ae2f3ad26f08accb
Vsevolod <sevakuzya@mail.ru>
2022-06-12 21:04:19 +0300

    Task 34 redone

--- ДЗ/ДЗ6/Homework5.py
+++ ДЗ/ДЗ6/Homework5.py
@@ -5,3 +5,12 @@ def get_info_from_file(filename):
     list_from_data = list(map(str, list_from_data.split()))
     data.close()
     return list_from_data
+
+def polynomial(lst):
+    x, y, z = [x for x in lst]
+    polynom = f"{x} * (x ** 2) + {y} * (x) + {str(z)} = 0"
+    return polynom
+
+def working_with_files (filename, modif, info):
+    with open(filename, modif) as data:
+        data.write(str(info))
--- ДЗ/ДЗ6/Homework6.py
+++ ДЗ/ДЗ6/Homework6.py
@@ -1,8 +1,30 @@
+# 34. Даны два файла в каждом из которых находится запись многочлена.
+# Сформировать файл содержащий сумму многочленов.
+
+
 from Homework5 import get_info_from_file
+from Homework5 import polynomial
+from Homework5 import working_with_files
 
 lst1 = get_info_from_file("Polynom1.txt")
 lst2 = get_info_from_file("Polynom2.txt")
-print(lst1)
-for i in range(len(lst1)):
-    if lst1[i] == "+":
-        print(lst1[i-1])
+
+def replacing1(lst):
+    rate_polynom= []
+    for i in lst:
+        if i != "+" and i != "0" and i != "=":
+            i = i.replace("*(x**2)","")
+            i = i.replace("*(x)","")
+            rate_polynom.append(i)
+    return rate_polynom
+
+def summ_of_polynom(lst1, lst2):
+    lst1 = list(map(int, lst1))
+    lst2 = list(map(int, lst2))
+    result = list(map(sum, zip(lst1, lst2)))
+    return result
+
+
+
+
+working_with_files("Result.txt", "w", polynomial(summ_of_polynom(replacing1(lst1), replacing1(lst2))))
